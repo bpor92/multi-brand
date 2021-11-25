d7c547d534a702d44bb44bf2ae0807ff68fb9090
Bartek P <bartosz.porebski@cloud-planet.pl>
2021-11-25 13:18:21 +0100

    add panel

--- module.brand1/src/components/Home.vue
+++ module.brand1/src/components/Home.vue
@@ -4,6 +4,7 @@
     <HelloWorld msg="Hello Vue 3 + Vite" />
     Brand1
     <router-link to="/test">Test</router-link>
+    <a href="/panel">panel</a>
     
   </div>
 </template>
--- module.brand1/vite.config.js
+++ module.brand1/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import commonConfig from '../vite.common.config'
+import history from 'vite-plugin-history'
+
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [...commonConfig.plugins],
+  plugins: [
+    ...commonConfig.plugins,
+    history(
+      { from: /\/panel/, to: '/panel/index.html' }
+    )
+  ],
   resolve: {
     alias: {
       ...commonConfig.resolve.alias,
