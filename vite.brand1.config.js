ab1ad5788c252422479510e190de4e8f110f252b
Bartek P <bartosz.porebski@cloud-planet.pl>
2021-12-23 11:16:37 +0100

    add nested module

--- vite.brand1.config.js
+++ vite.brand1.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import { prepareConfig } from './vite.common.config'
+const { resolve } = require('path')
 
 export default defineConfig({
   ...prepareConfig({ 
     brand: 'brand1',
     port: 3000
-  })
+  }),
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'module.brand1/index.html'),
+        nested: resolve(__dirname, 'module.brand1/about/index.html')
+      }
+    }
+  }
 })
